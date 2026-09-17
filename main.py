b87e2c79b50f549acd6fceb144cb38ff093c522d
fatenalqadere-creator <faten.alqadere@gmail.com>
2026-09-17 16:18:10 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+name = "Faten Al-Qadri"
+major = "Private Tutoring"
+university = "Not affiliated with a university"
+
+print("Student Profile")
+print("----------------")
+print("Name:", name)
+print("Major:", major)
+print("University:", university)
+print("Career Goal: To develop innovative educational and digital learning skills")
